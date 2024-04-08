a33165642ddc265d06fe594279a26d8b928b6b0c
Erum shahzadi <163527258+Erumshahzadi@users.noreply.github.com>
2024-04-08 11:23:23 +0500

    vacation work by Sir Hamza (9 to 12 am)

---
+++ main.ts
@@ -0,0 +1,8 @@
+// Question3: Write a program to determine the greater of two numbers.
+const num1:number=6;
+const num2:number=12;
+if(num1 > num2){
+    console.log(`Number1${num1}: is greater than Number2:${num2}`);
+}
+else
+console.log(`Number2:${num2}: is greater than Number1:${num1}`);
